609703056b79114d67db8752cc6cb6f0d2348b5f
Chris Proctor <chris@chrisproctor.net>
2022-02-06 20:40:13 -0500

    Initial commit

---
+++ hailstone_sequence.py
@@ -0,0 +1,8 @@
+print("--- Hailstone Sequence ---")
+
+n = -1
+while n <= 0:
+    n = int(input("Please enter a positive integer: "))
+
+# Replace the next line with your program.
+print("Sorry, this program doesn't work yet.")
